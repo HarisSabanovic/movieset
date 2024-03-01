ff5a6556751926edceafb0b73889a26fd45e7409
Haris Sabanovic <hasa2303@student.miun.se>
2024-03-01 13:57:36 +0100

    funktion som sker när sök knapp är tryckt

--- src/js/search.js
+++ src/js/search.js
@@ -4,32 +4,34 @@ let searchEl = document.getElementById("movie-input");
 let resultEl = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
 
-displayMovies();
 
-async function displayMovies(){
+searchBtn.addEventListener("click", async () => {
     //let userInput = searchEl.value;
-    let userInput = "Breaking bad";
+    let userInput = searchEl.value.trim();
+    if(userInput !== ""){
     const url = `http://www.omdbapi.com/?s=${userInput}&page=1&apikey=b69bb950`;
 
+    try{
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
 
+        resultEl.innerHTML = "";
+
         if (data.Response === "True") {
             // Iterate over each movie object in the "Search" array
-            const movies = data.Search.slice(0, 1);
+            const movies = data.Search.slice(0, 3);
             movies.forEach(movie => {
                 resultEl.innerHTML += 
                 `
                     <div class="info">
                         <img src=${movie.Poster} class="poster">
-                        <div>
-                            <h2>${movie.Title}</h2>
+                    
                             <div class="details">
+                                <h2>${movie.Title}</h2>
                                 <p>Type: ${movie.Type}</p>
                                 <p>Release Date: ${movie.Year}</p>
                             </div>
-                        </div>
                     </div>
                     
                 `;
@@ -38,4 +40,8 @@ async function displayMovies(){
             // Display a message if there are no results
             resultEl.innerHTML = "<p>No movies found</p>";
         }
-}
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+    
+}});
