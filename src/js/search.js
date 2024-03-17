db51a4c5380945fd02b33ab5d5d82a76e861fdec
Haris Sabanovic <hasa2303@student.miun.se>
2024-03-17 23:12:17 +0100

    små justeringar

--- src/js/search.js
+++ src/js/search.js
@@ -1,14 +1,17 @@
 "use strict";
 
+
 let searchEl = document.getElementById("movie-input");
 let resultEl = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
 
+const omdbApi = "b69bb950";
+const youtubeKey = "AIzaSyDYLEOK2NarC5Chv_5gk7wed3tOAHjdYds";
 
 searchBtn.addEventListener("click", async () => {
     let userInput = searchEl.value.trim();
     if (userInput !== "") {
-        const url = `https://www.omdbapi.com/?s=${userInput}&page=1&apikey=b69bb950`;
+        const url = `https://www.omdbapi.com/?s=${userInput}&page=1&apikey=${omdbApi}`;
 
         try {
             const response = await fetch(url);
@@ -19,19 +22,26 @@ searchBtn.addEventListener("click", async () => {
 
             if (data.Response === "True") {
                 const movies = data.Search.slice(0, 3);
+
+                //går igenom varje film i arrayen movies
+                //sen tilldelar varje film movie varje gång den körs
                 for (const movie of movies) {
                     const trailerId = await moviesApi(movie.Title, movie.Year);
                     const trailerUrl = `https://www.youtube-nocookie.com/embed/${trailerId}`;
                     resultEl.innerHTML += 
                     `
                         <div class="info">
-                            <img src=${movie.Poster} class="poster">
+        
+                            <img src=${movie.Poster}>
+                        
                             <div class="details">
                                 <h2>${movie.Title}</h2>
                                 <p>Type: ${movie.Type}</p>
                                 <p>Release Date: ${movie.Year}</p>
-                                <iframe width="560" height="315" src="${trailerUrl}" frameborder="0" allowfullscreen></iframe>
                             </div>
+                            <div class="trailer">
+                        <iframe width="560" height="315" src="${trailerUrl}" frameborder="0" allowfullscreen></iframe>
+                        </div>
                         </div>
                     `;
                 }
@@ -46,7 +56,6 @@ searchBtn.addEventListener("click", async () => {
 
 
 async function moviesApi(movieTitle, releaseYear) {
-    const youtubeKey = "AIzaSyDYLEOK2NarC5Chv_5gk7wed3tOAHjdYds";
     const youtubeUrl = `https://www.googleapis.com/youtube/v3/search?key=${youtubeKey}&part=snippet&type=video&q=${movieTitle} ${releaseYear} trailer`;
 
     try {
